db6bf5452c1af3cf021637d8c056d100d7634a55
Jay Yan Jia <yan1200@gmail.com>
2025-06-24 17:32:59 -0700

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Sync time
+echo "Sync time..."
 sudo sntp -sS time.apple.com
 
-# Check if Xcode Command Line Tools are installed
+echo "Check if Xcode Command Line Tools are installed..."
 if ! command -v xcode-select &> /dev/null; then
     echo "Xcode Command Line Tools not found. Installing..."
     xcode-select --install
@@ -11,12 +11,16 @@ else
     echo "Xcode Command Line Tools already installed."
 fi
 
-# Disable natural scroll direction
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+echo "Disabling Natural Scrolling..."
+defaults write -g com.apple.swipescrolldirection -bool false
 
-# Enable dark mode
+echo "Setting Mouse Speed to 2.0..."
+defaults write -g com.apple.mouse.scaling -float 2.0
+
+echo "Enable dark mode..."
 defaults write NSGlobalDomain AppleInterfaceStyle Dark
 
 # Restart necessary apps to apply changes
+echo "Applying changes..."
 killall SystemUIServer
 killall Finder
